6a8898fe95ba26ee85a5d85035c86e330c69a870
RationalGeek <jkohlhepp@gmail.com>
2014-05-08 08:47:28 -0400

    Initial commit of MVC app

---
+++ source/Bespoker.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bespoker.Web.MvcApplication" Language="C#" %>
---
+++ source/Bespoker.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bespoker.Web.Startup))]
+namespace Bespoker.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
