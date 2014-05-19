a3714d29a87a6af344e366c3df7c074743abac92
RationalGeek <jkohlhepp@gmail.com>
2014-05-19 15:18:18 -0400

    Basic SignalR back-and-forth

---
+++ source/Bespoker.Web/BespokerHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Bespoker.Web
+{
+    public class BespokerHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello(DateTime.Now.ToString("T"));
+        }
+    }
+}
--- source/Bespoker.Web/Startup.cs
+++ source/Bespoker.Web/Startup.cs
@@ -9,6 +9,8 @@ namespace Bespoker.Web
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            app.MapSignalR();
         }
     }
 }
