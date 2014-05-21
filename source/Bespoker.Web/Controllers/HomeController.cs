94605efadc87d536e87d7beef29f03d0ba22dc2f
RationalGeek <jkohlhepp@gmail.com>
2014-05-21 15:16:42 -0400

    Small look & feel changes

--- source/Bespoker.Web/Controllers/HomeController.cs
+++ source/Bespoker.Web/Controllers/HomeController.cs
@@ -21,15 +21,6 @@ namespace Bespoker.Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
