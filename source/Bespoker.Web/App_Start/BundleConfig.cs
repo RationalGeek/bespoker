d384d164b2c977f5be47db08df4745876e1d92f1
RationalGeek <jkohlhepp@gmail.com>
2014-05-21 15:59:29 -0400

    Added JSON2 to support IE 8

--- source/Bespoker.Web/App_Start/BundleConfig.cs
+++ source/Bespoker.Web/App_Start/BundleConfig.cs
@@ -7,6 +7,9 @@ namespace Bespoker.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/json2").Include(
+                        "~/Scripts/json2.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
